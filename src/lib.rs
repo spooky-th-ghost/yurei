42bbf315e1ac3af4c0cd7932da221cb525175103
spooky <spooky@pop-os.localdomain>
2022-12-27 23:07:53 -0500

    adds a stated version of the core plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::schedule::StateData, prelude::*};
 use bevy_rapier3d::prelude::*;
 
 pub mod components;
@@ -7,9 +7,39 @@ pub use components::*;
 pub mod systems;
 pub use systems::*;
 
+pub struct YureiPluginWithState<T>(T);
+
+impl<T> Plugin for YureiPluginWithState<T>
+where
+    T: StateData,
+{
+    fn build(&self, app: &mut App) {
+        app.add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+            .insert_resource(RapierConfiguration {
+                gravity: Vec3::Y * -30.0,
+                ..default()
+            })
+            .register_type::<Hover>()
+            .register_type::<Movement>()
+            .add_system_set(
+                SystemSet::on_update(self.0.clone())
+                    .with_system(handle_hover)
+                    .with_system(handle_rotation)
+                    .with_system(handle_moving_platforms)
+                    .with_system(handle_movement)
+                    .with_system(handle_damping.after(handle_movement)),
+            )
+            .add_system(handle_hover)
+            .add_system(handle_rotation)
+            .add_system(handle_moving_platforms)
+            .add_system(handle_movement)
+            .add_system(handle_damping.after(handle_movement));
+    }
+}
+
 pub struct YureiPlugin;
 
-impl Plugin for YureiPlugin {
+impl YureiPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
             .insert_resource(RapierConfiguration {
@@ -25,7 +55,6 @@ impl Plugin for YureiPlugin {
             .add_system(handle_damping.after(handle_movement));
     }
 }
-
 pub mod prelude {
     pub use crate::Grounded;
     pub use crate::Hover;
